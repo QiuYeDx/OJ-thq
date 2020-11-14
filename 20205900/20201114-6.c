53961ee8a5ead095b1a930b27484b1f7f553230a
QiuYeDx <20205900@stu.neu.edu.cn>
2020-11-14 19:31:24 +0800

    202011lib-ok

--- 20205900/20201114-6.c
+++ 20205900/20201114-6.c
@@ -3,29 +3,22 @@
 int main()
 {
 	int num[10]={0};
-	int min=0;
-	int vs=0;
-	int i_1=0;
-	int i_2=1;
-	int k=0;
-	int k_i=0;
+	int i=0,j=0,m=0,k=0;
 	
 	scanf("%d%d%d%d%d%d%d%d%d%d",&num[0],&num[1],&num[2],&num[3],&num[4],
            	&num[5],&num[6],&num[7],&num[8],&num[9]);
     
-    for(;k_i<=9;k_i++)
-    {
-		for(i_1=1,i_2=2;i_2<=10;i_1++,i_2++)
-		{
-			vs = num[i_1];
-			if(num[i_1]>=num[i_2])
-			{
-				num[i_1]=num[i_2];
-				num[i_2]=vs;
+    for(i=0;i<=8;i++)
+    	for(j=i+1;j<=9;j++)
+    	{
+    		if(num[i]>num[j])
+    		{
+    			m=num[i];
+    			num[i]=num[j];
+    			num[j]=m;
 			}
 		}
-		
-	}
+
 	for(k=0;k<=9;k++)
 	printf("%d\n",num[k]);
 	
